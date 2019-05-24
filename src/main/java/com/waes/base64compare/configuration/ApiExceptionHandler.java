f4202b5dbbfab08620d47bbd4ce126f22310dc44
allanweber <a.cassianoweber@gmail.com>
2019-05-24 20:09:15 -0300

    Exception handler configuration. Exception class and DTO to return message

---
+++ src/main/java/com/waes/base64compare/configuration/ApiExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.waes.base64compare.configuration;
+
+import com.waes.base64compare.domain.dto.ExceptionResponse;
+import com.waes.base64compare.domain.exception.ApiException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = {
+            ApiException.class,
+            HttpMessageNotReadableException.class,
+            MethodArgumentNotValidException.class
+    })
+    protected ResponseEntity<ExceptionResponse> handleApiException(Exception ex) {
+        return new ResponseEntity<>(new ExceptionResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    protected ResponseEntity<ExceptionResponse> handleException(Exception ex) {
+        return new ResponseEntity<>(
+                new ExceptionResponse(String.format("Not well treated exception: %s - Exception Type: %s"
+                        , ex.getMessage(),
+                        ex.getClass())), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
