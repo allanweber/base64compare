b29635cdcc4ca1891bc22ec2984cfc2392b826c8
allanweber <a.cassianoweber@gmail.com>
2019-05-25 15:34:56 -0300

    base64 validation testes

---
+++ src/test/java/com/waes/base64compare/domain/validator/Base64ValidationIntegratedTest.java
@@ -0,0 +1,34 @@
+package com.waes.base64compare.domain.validator;
+
+import com.waes.base64compare.Application;
+import com.waes.base64compare.domain.dto.JsonData;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class Base64ValidationIntegratedTest {
+
+    @Autowired
+    public Validator validator;
+
+    @Test
+    public void shoul_return_validation_constraint_to_invalid_base64() {
+
+        JsonData data = new JsonData("%aaa%");
+
+        Set<ConstraintViolation<JsonData>> violations = validator.validate(data);
+
+        assertEquals("The value must be a valid base 64 string.", violations.iterator().next().getMessage());
+    }
+}
