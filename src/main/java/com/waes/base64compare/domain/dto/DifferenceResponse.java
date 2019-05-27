7977e1ac47392218601fd696ae0a0bc8bdd2dc7a
allan_weber <allan_weber@terceiros.sicredi.com.br>
2019-05-27 11:15:00 -0300

    Document classes

--- src/main/java/com/waes/base64compare/domain/dto/DifferenceResponse.java
+++ src/main/java/com/waes/base64compare/domain/dto/DifferenceResponse.java
@@ -3,26 +3,54 @@ package com.waes.base64compare.domain.dto;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * Response to get in API to check base64 differences.
+ */
 @JsonPropertyOrder({"id", "equal", "equalSize", "differences", "left", "right"})
 public class DifferenceResponse {
 
+    /**
+     * The id of transaction.
+     */
     private Long id;
 
+    /**
+     * Value of base64 on left side.
+     */
     private String left;
 
+    /**
+     * Value of base64 on right side.
+     */
     private String right;
 
+    /**
+     * Indicates if both sides are equals.
+     */
     private boolean equal;
 
+    /**
+     * Indicates if both sides have the same length.
+     */
     private boolean equalSize;
 
+    /**
+     * List of differences if arrays are not equal but have same length.
+     */
     private List<Difference> differences;
 
+    /**
+     * Only possible constructor setting common data.
+     * @param id is the id of the transaction.
+     * @param left value of left side.
+     * @param right value of right side.
+     */
     public DifferenceResponse(Long id, String left, String right) {
-        this.id = id;
-        this.left = left;
-        this.right = right;
+            this.id = Objects.requireNonNull(id, "Id is a required argument.");
+        this.left =  Objects.requireNonNull(left, "Left is a required argument.");
+        this.right =  Objects.requireNonNull(right, "Right is a required argument.");
     }
 
     public void setEqual(boolean equal) {
