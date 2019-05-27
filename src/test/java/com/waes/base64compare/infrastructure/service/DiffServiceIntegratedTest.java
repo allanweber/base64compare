4aff831aeea9023e22bdf6ea348ea01084cfae60
allan_weber <allan_weber@terceiros.sicredi.com.br>
2019-05-27 11:16:21 -0300

    Fix and implement tests

--- src/test/java/com/waes/base64compare/infrastructure/service/DiffServiceIntegratedTest.java
+++ src/test/java/com/waes/base64compare/infrastructure/service/DiffServiceIntegratedTest.java
@@ -1,6 +1,7 @@
 package com.waes.base64compare.infrastructure.service;
 
 import com.waes.base64compare.Application;
+import com.waes.base64compare.domain.dto.Difference;
 import com.waes.base64compare.domain.dto.DifferenceResponse;
 import com.waes.base64compare.domain.entity.DiffEntity;
 import com.waes.base64compare.domain.entity.Side;
@@ -29,11 +30,21 @@ public class DiffServiceIntegratedTest {
     public ExpectedException expectedException = ExpectedException.none();
 
     @Test
-    public void should_save_a_new_entity() {
+    public void should_throw_NullPointerException_getting_difference_there_is_not_right_side() {
         Long id = 100L;
         service.saveLeft(id, "YWxsYW4gY2Fzc2lhbm8gd2ViZXI=");
 
         expectedException.expectMessage("The entity 100 does not have the Right side.");
+        expectedException.expect(NullPointerException.class);
+        service.getDifferences(id);
+    }
+
+    @Test
+    public void should_throw_NullPointerException_getting_difference_there_is_not_left_side() {
+        Long id = 100L;
+        service.saveRight(id, "YWxsYW4gY2Fzc2lhbm8gd2ViZXI=");
+
+        expectedException.expectMessage("The entity 100 does not have the Left side.");
         expectedException.expect(NullPointerException.class);
         service.getDifferences(id);
     }
@@ -95,6 +106,10 @@ public class DiffServiceIntegratedTest {
         assertFalse(response.getEqual());
         assertTrue(response.getEqualSize());
 
-        assertNull(response.getDifferences());
+        assertNotNull(response.getDifferences());
+
+        Difference difference = response.getDifferences().iterator().next();
+        assertEquals(6,difference.getPosition());
+        assertEquals(14,difference.getOffSet());
     }
 }
