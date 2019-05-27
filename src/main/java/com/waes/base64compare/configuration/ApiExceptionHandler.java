6b8c42b48fbba0ffa9365fe870a779cd7f957c28
allan_weber <allan_weber@terceiros.sicredi.com.br>
2019-05-27 15:30:26 -0300

    Implement log4j

--- src/main/java/com/waes/base64compare/configuration/ApiExceptionHandler.java
+++ src/main/java/com/waes/base64compare/configuration/ApiExceptionHandler.java
@@ -4,6 +4,8 @@ import com.waes.base64compare.domain.dto.ExceptionResponse;
 import com.waes.base64compare.domain.exception.ApiException;
 import com.waes.base64compare.domain.exception.ArrayNotEqualException;
 import com.waes.base64compare.domain.exception.DataBaseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  */
 @ControllerAdvice
 public class ApiExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
     /**
      * Handle and format any treated application exception.
@@ -29,6 +33,9 @@ public class ApiExceptionHandler {
             ArrayNotEqualException.class
     })
     protected ResponseEntity<ExceptionResponse> handleApiException(Exception ex) {
+
+        LOGGER.error("Expected error: {}", ex.getMessage());
+
         return new ResponseEntity<>(new ExceptionResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
@@ -40,9 +47,10 @@ public class ApiExceptionHandler {
      */
     @ExceptionHandler(value = {Exception.class})
     protected ResponseEntity<ExceptionResponse> handleException(Exception ex) {
-        return new ResponseEntity<>(
-                new ExceptionResponse(String.format("Not well treated exception: %s - Exception Type: %s"
-                        , ex.getMessage(),
-                        ex.getClass())), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        String message = String.format("Not well treated exception: %s - Exception Type: %s", ex.getMessage(), ex.getClass());
+        LOGGER.error("Expected error: {}", message);
+
+        return new ResponseEntity<>(new ExceptionResponse(message), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
