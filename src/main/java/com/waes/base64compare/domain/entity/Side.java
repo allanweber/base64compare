1e0e499f8eb1cfd0dbde6cfaa492509eacaab25a
allanweber <a.cassianoweber@gmail.com>
2019-06-01 15:43:55 -0300

    Change getDifferences to use Functions

--- src/main/java/com/waes/base64compare/domain/entity/Side.java
+++ src/main/java/com/waes/base64compare/domain/entity/Side.java
@@ -4,6 +4,16 @@ package com.waes.base64compare.domain.entity;
  * Enum to select one side.
  */
 public enum Side {
-    Left,
-    Right
+    Left(0),
+    Right(1);
+
+    private int value;
+
+    Side(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
 }
--- src/main/java/com/waes/base64compare/infrastructure/service/DiffService.java
+++ src/main/java/com/waes/base64compare/infrastructure/service/DiffService.java
@@ -11,16 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Base64;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Service responsible to manage Diff entities and theirs sides.
  */
 @Service
 public class DiffService implements IDiffService {
+
+    private static final String message = "The entity %s does not have the %s side.";
 
     /**
      * Repository to save diff entity.
@@ -66,52 +68,91 @@ public class DiffService implements IDiffService {
     /**
      * Return the result of the comparison of the sides of the DiffEntity.
      * Throws exception if id does not exist or if one side is null or empty.
+     *
      * @param id is the is of the DiffEntity.
      * @return data regarding to difference evaluation.
      * @see com.waes.base64compare.domain.dto.DifferenceResponse
      */
     @Override
     public DifferenceResponse getDifferences(Long id) {
-        DiffEntity entity = repository.get(id);
-
-        //Throw exception if id does not exist
-        if (entity == null) {
-            throw new ApiException(String.format("Diff %s id does not exist.", id));
-        }
-
-        //Get both sides from DiffEntity
-        String leftBase64 = entity.getSide(Side.Left);
-        String rightBase64 = entity.getSide(Side.Right);
-
-        //Validate if both sides are fill or throw a exception if any are not fill.
-        String message = "The entity %s does not have the %s side.";
-        if (StringUtils.isEmpty(leftBase64)) {
-            throw new ApiException(String.format(message, id, Side.Left));
-        }
-        if (StringUtils.isEmpty(rightBase64)) {
-            throw new ApiException(String.format(message, id, Side.Right));
-        }
-
-        //Convert both sides to byte[] to check their equality or differences.
-        byte[] left = Base64.getDecoder().decode(leftBase64);
-        byte[] right = Base64.getDecoder().decode(rightBase64);
-
-        //Create basic response with common data.
-        //If not enter in any if statement below, then return that both sides are not equals and not have equal sizes.
-        DifferenceResponse response = new DifferenceResponse(id, leftBase64, rightBase64);
-
-        //If both sides are equal, only set it and finish the comparison.
-        if (java.util.Arrays.equals(left, right)) {
-            response.setEqual(true);
-        } else if (left.length == right.length) {
-            //If sides are different but have same size, then compare to find differences.
-            response.setEqualSize(true);
-            List<Difference> differences = ArrayComparator.getDifferences(left, right);
-            response.setDifferences(differences);
-        }
+        DifferenceResponse response;
+        response = selectById()
+                .andThen(checkEntity(id))
+                .andThen(checkLeft())
+                .andThen(checkRight())
+                .andThen(newDifferenceResponse())
+                .andThen(getSidesArrays())
+                .andThen(areEquals())
+                .andThen(areSameSize())
+                .apply(id);
 
         return response;
     }
 
+    private Function<Long, DiffEntity> selectById() {
+        return id -> repository.get(id);
+    }
 
+    private Function<DiffEntity, DiffEntity> checkEntity(Long id) {
+        return entity -> Optional.ofNullable(entity).orElseThrow(
+                () -> new ApiException(String.format("Diff %s id does not exist.", id)));
+    }
+
+    private Function<DiffEntity, DiffEntity> checkLeft() {
+        return entity -> {
+            if (StringUtils.isEmpty(entity.getSide(Side.Left))) {
+                throw new ApiException(String.format(message, entity.getId(), Side.Left));
+            }
+            return entity;
+        };
+    }
+
+    private Function<DiffEntity, DiffEntity> checkRight() {
+        return entity -> {
+            if (StringUtils.isEmpty(entity.getSide(Side.Right))) {
+                throw new ApiException(String.format(message, entity.getId(), Side.Right));
+            }
+            return entity;
+        };
+    }
+
+    private Function<DiffEntity, DifferenceResponse> newDifferenceResponse() {
+        return entity ->
+                new DifferenceResponse(entity.getId(), entity.getSide(Side.Left), entity.getSide(Side.Right));
+    }
+
+    private Function<DifferenceResponse, DifferenceResponse> getSidesArrays() {
+        return response -> {
+            byte[] left = Base64.getDecoder().decode(response.getLeft());
+            byte[] right = Base64.getDecoder().decode(response.getRight());
+            response.setSides(Arrays.asList(left, right));
+            return response;
+        };
+    }
+
+    private Function<DifferenceResponse, DifferenceResponse> areEquals() {
+        return response -> {
+            if (java.util.Arrays.equals(
+                    response.getSides().get(Side.Left.value()),
+                    response.getSides().get(Side.Right.value()))) {
+                response.setEqual(true);
+            }
+            return response;
+        };
+    }
+
+    private Function<DifferenceResponse, DifferenceResponse> areSameSize() {
+        return response -> {
+            if (response.getEqual()) return response;
+            byte[] left = response.getSides().get(Side.Left.value());
+            byte[] right = response.getSides().get(Side.Right.value());
+            if (left.length == right.length) {
+                //If sides are different but have same size, then compare to find differences.
+                response.setEqualSize(true);
+                List<Difference> differences = ArrayComparator.getDifferences(left, right);
+                response.setDifferences(differences);
+            }
+            return response;
+        };
+    }
 }
